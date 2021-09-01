2fbd90bb27be112d09254e6bcaf53eaf58ed89ab
ronald <ronaldgutierrezj@gmail.com>
2021-09-01 18:30:27 -0500

    refresh previous DOM elements created

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ let queenInfo;
 const $name = $('#name');
 const $winner = $('#winner');
 const $quote = $('#quote');
-const $photo = $('#photo')
+const $photo = $('#photo');
+
+
 
 const $input = $('input[type="text"]')
 
 function handleGetData (event){
     event.preventDefault() 
+    $('.dom').remove()
     $.ajax({
         url:`https://www.nokeynoshade.party/api/queens?name=${$input.val()}`
       }).then(
+          
         function(data){    
          queenInfo = data;
+
+         $input.val('')
          render();
 
         },
@@ -27,7 +33,22 @@ function handleGetData (event){
         $name.text(queenInfo[0].name);
         $winner.text(queenInfo[0].winner);
         $quote.text(queenInfo[0].quote);
-        $photo.attr("src", queenInfo[0].image_url)
+        $photo.attr("src", queenInfo[0].image_url);
+        queenInfo[0].seasons.forEach(element => {
+            
+            let div = document.createElement('div');
+            div.className= 'dom'
+            let main = document.querySelector('main');
+            main.children[2].insertAdjacentElement("afterend",div)
+            let newH2 = document.createElement('h2');
+            newH2.textContent = "Season";
+            div.appendChild(newH2);
+            let newh3 = document.createElement('h3');
+            newh3.textContent= element.seasonNumber;
+            div.appendChild(newh3)
+
+            
+        });
         
         
     }
